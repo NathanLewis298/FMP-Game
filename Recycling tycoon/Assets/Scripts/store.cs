836d5b87e646163872e26378f14a36da474acf58
Big_Earl <477117@chrome.escg.ac.uk>
2020-03-02 16:30:58 +0000

    Coding the money balance and the first money maker

---
+++ Recycling tycoon/Assets/Scripts/store.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class store : MonoBehaviour
+{
+    float CurrentBalance;
+    float BaseStoreCost;
+    int StoreCount;
+    public Text StoreCountText;
+    public Text CurrentBalanceText;
+
+    void Start()
+    {
+        StoreCount = 1;
+        CurrentBalance = 2.0f;
+        BaseStoreCost = 1.50f;
+        CurrentBalanceText.text = CurrentBalance.ToString("C2");
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void BuyStoreOnClick ()
+    {
+        if (BaseStoreCost > CurrentBalance)
+            return;
+        StoreCount = StoreCount + 1;
+        Debug.Log(StoreCount);
+        StoreCountText.text = StoreCount.ToString();
+        CurrentBalance = CurrentBalance - BaseStoreCost;
+        Debug.Log(CurrentBalance);
+        CurrentBalanceText.text = CurrentBalance.ToString("C2");
+    }
+
+
+}
