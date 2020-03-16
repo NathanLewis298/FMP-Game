ca2e59cc18ab5c391421b70117c129ae3a563013
Big_Earl <477117@chrome.escg.ac.uk>
2020-03-16 16:31:21 +0000

    half working upgrade system

---
+++ Recycling tycoon/Assets/Scripts/UpgradeShop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeShop : MonoBehaviour
+{
+    public Store storeToUpgrade;
+    public MoneyHandler moneyHandler;
+    public int[] numberOfUpgrades = { 2, 3, 4, 5 };
+    public float[] upgradeCost = new float[4];
+
+    public float startCost = 500f;
+    public float costMultiplier = 3f;
+
+    int curentUpgrade = 0;
+
+    private void Start()
+    {
+        for (int i = 0; i < upgradeCost.Length; i++)
+        {
+            if (i == 0)
+            {
+                upgradeCost[i] = startCost;
+            }
+            else
+            {
+                upgradeCost[i] = costMultiplier * upgradeCost[i - 1];
+            }
+
+        }
+    }
+
+    public void Upgrade()
+    {
+        if (moneyHandler.totalMoney >= upgradeCost[curentUpgrade])
+        {
+            storeToUpgrade.totalValue *= numberOfUpgrades[curentUpgrade];
+            storeToUpgrade.baseStoreProfit *= numberOfUpgrades[curentUpgrade];
+            moneyHandler.totalMoney -= upgradeCost[curentUpgrade];
+            storeToUpgrade.DisplayPrices(storeToUpgrade.amountGenText, storeToUpgrade.totalValue);
+            curentUpgrade++;
+        }
+    }
+}
---
+++ Recycling tycoon/Assets/Scripts/Upgrademenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrademenu : MonoBehaviour
+{
+    [SerializeField] private GameObject upgradeMenuUI;
+
+
+
+
+    public void ActivateMenu()
+    {
+        upgradeMenuUI.SetActive(true);
+    }
+
+
+    public void DeactivateMenu()
+    {
+        upgradeMenuUI.SetActive(false);
+        
+    }
+}
